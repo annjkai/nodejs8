e0df8d0e29e520e45075de9b7abbabb16b5608c4
annika <ironannebonnie@gmail.com>
2019-02-26 16:52:03 +0100

    chapter 4

---
+++ microservices/zmq-watcher-sub.js
@@ -0,0 +1,19 @@
+'use strict'
+
+const zmq = require('zeromq')
+
+//create subscriber endpoint
+const subscriber = zmq.socket('sub')
+
+//subscribe to all messages
+subscriber.subscribe('')
+
+//handle messages from publisher
+subscriber.on('message', data => {
+    const message = JSON.parse(data)
+    const date = new Date(message.timestamp)
+    console.log(`File "${message.file}" changed at ${date}`)
+})
+
+//connect to publisher
+subscriber.connect("tcp://localhost:60500")
