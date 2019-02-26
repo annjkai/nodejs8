c3a612935f4ec8fe2abf28c824759c6e913ab9f5
annika <ironannebonnie@gmail.com>
2019-02-26 18:01:35 +0100

    chapter 4

---
+++ microservices/zmq-filer-rep.js
@@ -0,0 +1,36 @@
+'use strict'
+
+const fs = require('fs')
+const zmq = require('zeromq')
+
+//socket to reply to client requests
+const responder = zmq.socket('rep')
+
+//handle incoming requests
+responder.on('message', data => {
+    
+    //parse incoming message
+    const request = JSON.parse(data)
+    console.log(`Received request to get: ${request.path}`)
+    
+    //read file & reply with content
+    fs.readFile(request.path, (err, content) => {
+        console.log('Sending response content.')
+        responder.send(JSON.stringify({
+            content: content.toString(),
+            timestamp: Date.now(),
+            pid: process.pid
+        }))
+    })
+})
+
+//listen on tcp port 60401
+responder.bind('tcp://127.0.0.1:60401', err => {
+    console.log('Listening for zmq requesters...')
+})
+
+//close responder when Node process ends
+process.on('SIGINT', () => {
+    console.log('Shutting down...')
+    responder.close()
+})
---
+++ microservices/zmq-filer-req.js
@@ -0,0 +1,19 @@
+'use strict'
+
+const zmq = require('zeromq')
+const filename = process.argv[2]
+
+//create request endpoint
+const requester = zmq.socket('req')
+
+//handle replies from responder
+requester.on('message', data => {
+    const response = JSON.parse(data)
+    console.log('Received response: ', response)
+})
+
+requester.connect('tcp://localhost:60401')
+
+//send request for content
+console.log(`Sending a request for ${filename}`)
+requester.send(JSON.stringify({ path: filename }))
